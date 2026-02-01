3740ea8d4c4be8f021f64da3cee741b58e14418a
nixonpoh <134215035+nixonpoh@users.noreply.github.com>
2026-02-01 13:46:16 +0000

    debug

--- app/auth/[...nextauth]/route.js
+++ app/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
+const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -30,6 +30,8 @@ const handler = NextAuth({
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
-});
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
